7ac3c780dd2e3c543b36ef5a9b556c91ff7ad740
JohnSatomura <saxifrageofyou@gmail.com>
2020-08-12 02:10:30 +0900

    Corrects that a comment must be pressed twice to be inserted

--- script/make-stamp.js
+++ script/make-stamp.js
@@ -1,6 +1,6 @@
 console.log("kokosuki : Start");
 
-// url 変更毎に実行
+// url Run on every change
 window.addEventListener('yt-navigate-start', process);
 
 if (document.body) process();
@@ -12,6 +12,7 @@ function process() {
     $("#stamp-button").remove();
     $("#menu-container").before("<div><button id=\"stamp-button\">❤</button></div>");
     
+    // Is the position of the function correct?
     $('#stamp-button').on('click', function(){
         console.log("button Click");
         write_comment_play_time();
@@ -19,24 +20,25 @@ function process() {
     });
 }
 
-//17 : control 81 : q 121 F10 F7,8,9あたりは空いてそう
-var map = {17: false, 81: false, 121: false};
+//17 : control 81 : q 121 F10 F7,8,9
+var map = {121: false};
 $(document).keydown(function(e) {
-    if (e.keyCode in map) {
-        map[e.keyCode] = true;
-        //swtch more better  ?
-        if (map[17] && map[81]) {
-            write_comment_play_time();
-        }else if(map[121]){
-            write_comment_play_time();
-        }
-    }
+    keycheck(e);
 }).keyup(function(e) {
-    map[17] = false;
-    map[81] = false;
-    map[121] = false;
+    keycheck(e);
 });
 
+function keycheck(e){
+    if (e.keyCode in map) {
+        map[e.keyCode] = true;
+        if(map[121]){//swtch more better  ?
+            write_comment_play_time();
+            map[121] = false;
+        }
+    }
+}
+
+// zero fill input, fillspace
 function zeroPadding(num,length){
     return ('0000000000' + num).slice(-length);
 }
@@ -45,11 +47,11 @@ function write_comment_play_time(){
     //console.log("wasWrittenFlag Play :" + wasWrittenFlag.toString());
     var getCurrentTime = document.getElementsByTagName("video");
     var currentTime = getCurrentTime[0].currentTime;//get second display -> 2min : output -> 120sec
-    
     var hour = zeroPadding(parseInt(Math.floor(currentTime)/60/60), 2);
     var min = zeroPadding(parseInt(Math.floor(currentTime)/60)%60, 2);
     var sec = zeroPadding(parseInt(Math.floor(currentTime))%60, 2);
     document.getElementById("simplebox-placeholder").click(); //なければ都バス
+    
     
     var previousComment = document.getElementById("contenteditable-root").innerText;
     var newLine =  (previousComment=="") ? "" : "\n";
@@ -57,6 +59,7 @@ function write_comment_play_time(){
     var insertComment = (hour == "00") ? previousComment + newLine + min + ":" + sec + " : " : previousComment + newLine + hour  + ":"  + min + ":" + sec + " : ";
     document.getElementById("contenteditable-root").innerHTML = insertComment;
     
+    //todo
     //毎回 コメントを保存しいて
     //popup に表示する、不要になれば削除ボタン
 }
