0b51bcc566b3c4707824095220fdb0fb8ca6d2d6
JohnSatomura <saxifrageofyou@gmail.com>
2020-09-22 01:20:04 +0900

    found missing source

--- script/popup.js
+++ script/popup.js
@@ -1,10 +1,8 @@
-$(function(){
-    $("#display").click(function () {
-        chrome.storage.local.get(['current_title'], function (value) { 
-            chrome.storage.local.get(['current_comment'], function (value) {
-                var insert_text = document.getElementById("main_textarea");
-                insert_text.textContent = "再生時間 \n" + value.current_comment;
-            });
+document.getElementById("display").onclick = function() {
+    chrome.storage.local.get(['current_title'], function (value) { 
+        chrome.storage.local.get(['current_comment'], function (value) {
+            var insert_text = document.getElementById("main_textarea");
+            insert_text.textContent = "再生時間 \n" + value.current_comment;
         });
     });
-});
+};
